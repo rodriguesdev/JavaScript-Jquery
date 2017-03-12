b3030c7974da026c4c9c9812a9e2304f62f0f540
Leandro Rodrigues <leandro.lacr@yahoo.com.br>
2017-03-12 12:43:22 -0300

    Incluindo um arquivo JavaScript na pagina
    Agora que temos algumas funções prontas e alguma funcionalidade, é uma
    boa
    prática separarmos o código JavaScript em um outro arquivo. Vamos chamar
    de
    rogus.js, e o gravamos no diretório javascripts.

---
+++ CasaDoCodigo.JavaScriptJquery/CasaDoCodigo.JavaScriptJquery/Controllers/GetCarrinhoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CasaDoCodigo.JavaScriptJquery.Controllers
+{
+    public class GetCarrinhoController : Controller
+    {
+        // GET: GetCarrinho
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CasaDoCodigo.JavaScriptJquery/CasaDoCodigo.JavaScriptJquery/Scripts/rogus.js
@@ -0,0 +1,23 @@
+﻿var total = document.getElementById('total');
+alert(total.innerHTML);
+
+function MoneyTextToFloat(text) {
+    var cleanText = text.replace("R$", "").replace(",", ".");
+    return parseFloat(cleanText);
+};
+
+function floatToMoneyText(value) {
+    var text = (value < 1 ? "0" : "") + Math.floor(value * 100);
+    text = "R$" + text;
+    return text.substr(0, text.length - 2) + "," + text.substr(-2);
+};
+
+function ReadTotal() {
+    var total = document.getElementById('total');
+    return moneyTextToFloat(total.innerHTML);
+};
+
+function writeTotal(value) {
+    var total = document.getElementById('total');
+    total.innerHTML = floatToMoneyText(value);
+};
